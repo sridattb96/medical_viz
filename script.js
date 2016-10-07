e455aa76b9d5260faad9de81234b86a4c2504762
Sridatt Bhamidipati <sridattbhamidipati@campus-081-033.ucdavis.edu>
2016-10-06 18:16:19 -0700

    set up env

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+
 console.log("test")
 
 d3.csv("data/antimicrobial.csv", function(data) {
-  console.log(data[0]);
+  console.log(data[0]["Dose"]);
+});
+
+d3.csv("data/sample.csv", function(data) {
+  console.log(data[1]);
+});
+
+d3.csv("data/enrollment.csv", function(data) {
+  console.log(data[3]);
 });
